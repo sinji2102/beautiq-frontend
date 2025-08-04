a01329bd7d435ef5712464d15ba683b67d32a892
박현빈 <105339034+HyunisEmpty@users.noreply.github.com>
2025-08-04 21:11:47 +0900

    Feat: Add simple-import-sort plugin to ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,33 +4,29 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
-import simpleImportSort from 'eslint-plugin-simple-import-sort' // import 정렬 플러그인 추가 
+import simpleImportSort from 'eslint-plugin-simple-import-sort' 
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
 
-    // extends: 여러 규칙 세트를 불러와서 적용합니다. 
     extends: [
-      js.configs.recommended, // JavaScript 기본 규칙
-      tseslint.configs.recommended,   // TypeScript 기본 규칙
-      reactHooks.configs['recommended-latest'], // React Hooks 규칙
-      reactRefresh.configs.vite,  // React Refresh 규칙
+      js.configs.recommended, 
+      tseslint.configs.recommended, 
+      reactHooks.configs['recommended-latest'], 
+      reactRefresh.configs.vite, 
     ],
 
-    // languageOptions: ECMAScript 버전, 글로벌 객체 등 환경 설정
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
 
-    // plugins: 추가 ESLint 플러그인 설정
     plugins: {
       'simple-import-sort': simpleImportSort,
     },
 
-    // rules: ESLint 규칙 설정
     rules:{
       'simple-import-sort/imports': 'warn',
       'simple-import-sort/exports': 'warn',
