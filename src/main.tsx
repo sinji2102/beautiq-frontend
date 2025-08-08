d7699acdc9b298cf21103b0fc3651561beb8001a
sinji <sinzi031012@naver.com>
2025-08-08 20:37:54 +0900

    init: rem 설정 및 모바일뷰 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppRouter from './routes/Router'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRouter from "./routes/Router";
+import "./index.css";
+import { Global } from "@emotion/react";
+import { globalStyles } from "./styles/global";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <AppRouter />
   </React.StrictMode>
-)
+);
