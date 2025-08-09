46d9a71c4d8e32a66dff6c3b6729cfbf7314d7db
sinji <sinzi031012@naver.com>
2025-08-10 03:32:58 +0900

    Init: 색상 및 폰트 디자인 시스템 세팅

--- src/pages/Apage/Apage.tsx
+++ src/pages/Apage/Apage.tsx
@@ -1,6 +1,12 @@
-import { TempSection } from "./APage.styled";
+import * as S from "./APage.styled";
 
 function Apage() {
-  return <TempSection>Apage입니다</TempSection>;
+  return (
+    <>
+      <S.TempHeader>헤더입니다</S.TempHeader>
+      <S.TempBody>바디입니다</S.TempBody>
+      <S.TempCaption>캡션입니다</S.TempCaption>
+    </>
+  );
 }
 export default Apage;
