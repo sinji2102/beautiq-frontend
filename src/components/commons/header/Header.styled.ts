315bca40b308691070f6dae12deb667600f9e883
HyunisEmpty <empty6004@kookmin.ac.kr>
2025-09-09 18:40:34 +0900

    Style: 코드 스타일 변경

--- src/components/commons/header/Header.styled.ts
+++ src/components/commons/header/Header.styled.ts
@@ -5,68 +5,68 @@ import { getTypography } from "../../../styles/typography";
 
 // 헤더 전체 래퍼
 export const HeaderWrapper = styled.div`
-    ${getTypography("heading3")}; 
-    background-color: ${({theme}) => theme.colors.neutral[100]};
-    color: ${({theme}) => theme.colors.primary[500]};  
+  ${getTypography("heading3")};
+  background-color: ${({ theme }) => theme.colors.neutral[100]};
+  color: ${({ theme }) => theme.colors.primary[500]};
 
-    width: 100%;
-    height: 56px;
-    padding: 12px;
-    margin-bottom: 16px;
+  width: 100%;
+  height: 56px;
+  padding: 12px;
+  margin-bottom: 16px;
 
-    position: sticky;
-    top: 0;
-    z-index: 10;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    box-sizing: border-box;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
 `;
 
 export const IconButton = styled.button`
-    all: unset;
-    cursor: pointer;
+  all: unset;
+  cursor: pointer;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    &:focus {
+  &:focus {
     outline: none;
-    }
+  }
 `;
 
 // 뒤로가기 아이콘
 export const BackIcon = styled(IconBack)`
-    width: 32px;
-    height: 32px;
-    fill: ${({theme}) => theme.colors.primary[100]};
+  width: 32px;
+  height: 32px;
+  fill: ${({ theme }) => theme.colors.primary[100]};
 `;
 
 // 메인 로고
 export const BeautiqIcon = styled(IconBeautiq)`
-    width: 82px;
-    height: 32px;
-    fill: ${({theme}) => theme.colors.primary[100]};
+  width: 82px;
+  height: 32px;
+  fill: ${({ theme }) => theme.colors.primary[100]};
 `;
 
 // 햄버거 아이콘
 export const HamburgerIcon = styled(IconHamburger)`
-    width: 32px;
-    height: 32px;
-    fill: ${({theme}) => theme.colors.primary[100]};
+  width: 32px;
+  height: 32px;
+  fill: ${({ theme }) => theme.colors.primary[100]};
 `;
 
 // 닫기 아이콘 -> 다른 아이콘과 크기 맞추기 위해 패딩 추가
 export const CloseIcon = styled(IconClose)`
-    width: 24px;
-    height: 24px;
-    padding: 4px;
-    fill: ${({theme}) => theme.colors.primary[100]};
+  width: 24px;
+  height: 24px;
+  padding: 4px;
+  fill: ${({ theme }) => theme.colors.primary[100]};
 `;
 
 // 빈공간 아이콘
 export const PlaceholderIcon = styled.div`
-    width: 32px;
-    height: 32px;
+  width: 32px;
+  height: 32px;
 `;
