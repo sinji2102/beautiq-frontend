3673e5aae79102d9cac3072b7fb6ecd10d302b96
HyunisEmpty <empty6004@kookmin.ac.kr>
2025-09-09 18:40:34 +0900

    Style: 코드 스타일 변경

--- src/pages/HeaderPage.tsx
+++ src/pages/HeaderPage.tsx
@@ -1,23 +1,20 @@
 import Header from "../components/commons/header/Header";
 
-function Headerpage(){
-    return (
-        // return문 안에는 태그 2개 반환 못해서 div로 감싸줘야함
-        <div>
-
-            <Header left="beautiq" right="hambuger"/>
-            <Header text="텍스트" right="hambuger"/>
-            <Header text="텍스트" right="close"/>
-            <Header left="back" text="텍스트"/>
-            <Header left="back" text="텍스트" right="close"/>
-            <Header right="hambuger"/>
-            <Header right="close"/>
-            <Header left="back" />
-            <Header left="back"  right="close"/>
-
-
-        </div>
-    );
+function Headerpage() {
+  return (
+    // return문 안에는 태그 2개 반환 못해서 div로 감싸줘야함
+    <div>
+      <Header left="beautiq" right="hambuger" />
+      <Header text="텍스트" right="hambuger" />
+      <Header text="텍스트" right="close" />
+      <Header left="back" text="텍스트" />
+      <Header left="back" text="텍스트" right="close" />
+      <Header right="hambuger" />
+      <Header right="close" />
+      <Header left="back" />
+      <Header left="back" right="close" />
+    </div>
+  );
 }
 
 export default Headerpage;
