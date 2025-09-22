4cb502e0c3a9abf7acd4d78d740c514d8efc2bad
sinji <sinzi031012@naver.com>
2025-09-22 23:21:36 +0900

    Feat: 홈 페이지 헤더와 사이드바 연결

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,6 @@
 import Header from "@components/commons/header/Header";
+import Sidebar from "@components/commons/sidebar/Sidebar";
+import { useState } from "react";
 
 import CardContainer from "./components/CardContainer/CardContainer";
 import RecentAnalysisCard from "./components/RecentAnalysisCard/RecentAnalysisCard";
@@ -6,15 +8,22 @@ import TodayTip from "./components/TodayTip/TodayTip";
 import * as S from "./HomePage.styled";
 
 const HomePage = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const handleClickSidebar = () => {
+    setSidebar((prev) => !prev);
+  };
+
   return (
     <>
-      <Header left="beautiq" right="hambuger" />
+      <Header left="beautiq" right="hambuger" handleClickHamburger={handleClickSidebar} />
       <S.HomePageWrapper>
         <S.HomeImage />
         <CardContainer />
         <RecentAnalysisCard />
         <TodayTip />
       </S.HomePageWrapper>
+      <Sidebar isOpen={sidebar} onClose={handleClickSidebar} />
     </>
   );
 };
