86eae9c8c525ba9a94c97d4161773bbe207f2316
sinji <sinzi031012@naver.com>
2025-09-22 20:09:33 +0900

    Feat: CardContainer 퍼블리싱

---
+++ src/assets/svgs/IconLotionBottle.tsx
@@ -0,0 +1,30 @@
+import type { SVGProps } from "react";
+import * as React from "react";
+const SvgIconLotionBottle = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 44 44"
+    {...props}
+  >
+    <path
+      fill="#F8312F"
+      d="M19.25 8.938V5.5h5.5v3.438c.004.687.693.88.693.88L22 11.688 18.556 9.82s.695-.195.694-.883"
+    />
+    <path
+      fill="#FBB8AB"
+      d="M9.625 20.625a11 11 0 0 1 11-11h2.75a11 11 0 0 1 11 11V35.75a5.5 5.5 0 0 1-5.5 5.5h-13.75a5.5 5.5 0 0 1-5.5-5.5z"
+    />
+    <path
+      fill="#F3EEF8"
+      d="M23.375 19.25s-1.375-2.75-2.75 0c0 0-4.125 6.188-4.125 10.313 0 1.375.688 4.812 5.5 4.812s5.5-3.437 5.5-4.812c0-4.125-4.125-10.313-4.125-10.313"
+    />
+    <path
+      fill="#FF8687"
+      d="M19.25 6.875C16.5 5.5 17.875 2.75 20.625 2.75c1.375 0 8.25 1.375 8.25 1.375 2.063.688 1.375 2.75 0 2.75z"
+    />
+  </svg>
+);
+export default SvgIconLotionBottle;
---
+++ src/pages/home/components/RecentAnalysisCard/RecentAnalysisCard.tsx
@@ -0,0 +1,11 @@
+import * as S from "./RecentAnalysisCard.styled";
+
+const RecentAnalysisCard = () => {
+  return (
+    <S.CardContainer>
+      <S.CardTitle>최근 분석 결과</S.CardTitle>
+    </S.CardContainer>
+  );
+};
+
+export default RecentAnalysisCard;
