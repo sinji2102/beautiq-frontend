4ed82a2011c55d246aea45f4f12b76f858eac83f
sinji <sinzi031012@naver.com>
2025-09-22 22:08:12 +0900

    Feat: 최근 분석 결과 박스 퍼블리싱

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,5 +1,6 @@
 export { default as IconBack } from "./IconBack";
 export { default as IconBeautiq } from "./IconBeautiq";
+export { default as IconBubbles } from "./IconBubbles";
 export { default as IconClose } from "./IconClose";
 export { default as IconHamburger } from "./IconHamburger";
 export { default as IconLipstick } from "./IconLipstick";
--- src/pages/home/components/RecentAnalysisCard/RecentAnalysisCard.styled.ts
+++ src/pages/home/components/RecentAnalysisCard/RecentAnalysisCard.styled.ts
@@ -1,8 +1,11 @@
+import { IconBubbles } from "@assets/svgs";
+import type { Theme } from "@emotion/react";
 import styled from "@emotion/styled";
 import { getTypography } from "@styles/typography";
 
 export const CardContainer = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 0.8rem;
   margin: 1.2rem 2.4rem;
   padding: 2.6rem 2rem;
@@ -13,5 +16,97 @@ export const CardContainer = styled.div`
 
 export const CardTitle = styled.h1`
   all: unset;
+  display: flex;
+  gap: 0.4rem;
+  align-items: center;
+
   ${getTypography("body1NormalSemi")}
+`;
+
+export const BubblesIcon = styled(IconBubbles)`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+export const ResultBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 5rem;
+`;
+
+export const ItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Title = styled.span`
+  color: ${({ theme }) => theme.colors.black};
+
+  ${getTypography("caption1Semi")}
+`;
+
+export const DateText = styled.span`
+  color: ${({ theme }) => theme.colors.gray[500]};
+
+  ${getTypography("caption2Medi")}
+`;
+
+// TODO : 피그마에서 디자인 변경하고 반영하기
+export const BadgeContainer = styled.div`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+const getBackgroundColorByScore = (theme: Theme, score?: number) => {
+  if (score == null) {
+    return theme.colors.primary[500];
+  }
+
+  if (score <= 30) return theme.colors.primary[900];
+  if (score <= 60) return theme.colors.primary[600];
+  return theme.colors.primary[200];
+};
+
+export const Badge = styled.div<{ score?: number }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.2rem 1.6rem;
+
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: 500;
+  font-size: 1.3rem;
+
+  background-color: ${({ theme, score }) => getBackgroundColorByScore(theme, score)};
+  border-radius: 4rem;
+
+  ${getTypography("caption2Medi")}
+`;
+
+export const ScoreBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  align-items: end;
+`;
+
+export const ScoreText = styled.span`
+  color: ${({ theme }) => theme.colors.gray[700]};
+  ${getTypography("caption2Medi")}
+`;
+
+export const FullHistoryButton = styled.button`
+  width: 100%;
+  padding: 1rem;
+
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 1.4rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.neutral[200]};
+  border-radius: 0.8rem;
+
+  ${getTypography("body2NormalSemi")}
 `;
