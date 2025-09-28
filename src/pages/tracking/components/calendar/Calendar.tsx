47c9bce9f784f4ea364acbfc7957791baee21013
박현빈 <105339034+HyunisEmpty@users.noreply.github.com>
2025-09-29 00:46:14 +0900

    Design: Calendar 컴포넌트에 pretender 폰트와 컬러 적용

--- src/pages/tracking/components/calendar/Calendar.tsx
+++ src/pages/tracking/components/calendar/Calendar.tsx
@@ -1,31 +1,27 @@
-// MUI 라이브러리 스타일 적용하는 컴포넌트? -> 테마 적용
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-// MUI 라이브러리가 한국 시간과 날짜를 달력에 적용하기 위한 컴포넌트들
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import dayjs, { Dayjs } from "dayjs";
+import { muiTheme } from "@styles/theme";
+import dayjs, { type Dayjs } from "dayjs";
 import { useState } from "react";
 
 import { StyledStaticDatePicker } from "./Calendar.styled";
-
-const theme = createTheme();
 
 const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs());
 
   return (
-    <LocalizationProvider dateAdapter = {AdapterDayjs}>
-      <ThemeProvider theme={theme}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <MuiThemeProvider theme={muiTheme}>
         <StyledStaticDatePicker
           displayStaticWrapperAs="desktop"
           value={selectedDate}
           onChange={(newDate) => setSelectedDate(newDate)}
-          // ActionBar를 완전히 제거 -> styled 에서는 제거할 방법을 못찾음
           slots={{
-            actionBar: () => null, 
+            actionBar: () => null,
           }}
         />
-      </ThemeProvider>
+      </MuiThemeProvider>
     </LocalizationProvider>
   );
 };
