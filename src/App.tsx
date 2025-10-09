7cf4977996b39bd5985dea44cb62ecf0b3fd4581
HyunisEmpty <empty6004@kookmin.ac.kr>
2025-10-09 15:35:57 +0900

    Feat: 분석 결과 일별 조회 API 연결에 대비한 임시 데이타및 기능 정의(개별 날짜에 2회 이상 검사한 경우 모달 컴포너트로 검사 결과 선택할 수 있도록 정의)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 
+import Modal from "@components/commons/modal/Modal";
 import { Global, ThemeProvider } from "@emotion/react";
 import router from "@routes/Router";
 import { globalStyles } from "@styles/global";
@@ -11,6 +12,7 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <Global styles={(theme) => globalStyles(theme as ThemeType)} />
+        <Modal />
         <RouterProvider router={router} />
       </ThemeProvider>
     </>
