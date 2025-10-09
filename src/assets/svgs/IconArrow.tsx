c4f9aa03e91136ca0315ca13f12b972957a25be9
sinji <sinzi031012@naver.com>
2025-10-09 14:53:17 +0900

    Chore: yarn svgr 실행 시 react import 하지 않도록 수정

--- src/assets/svgs/IconArrow.tsx
+++ src/assets/svgs/IconArrow.tsx
@@ -1,14 +1,6 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgIconArrow = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    fill="none"
-    viewBox="0 0 24 24"
-    {...props}
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
     <path
       fill="#E46198"
       fillRule="evenodd"
