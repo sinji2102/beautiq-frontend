11422172d5e6251a6717d117524b734b89b86eea
sinji <sinzi031012@naver.com>
2025-10-28 17:47:19 +0900

    design: 헤더 뒤로가기 버튼 레이아웃 안 맞는 이슈 수정

---
+++ src/pages/RecommendStyle/RecommendStylePage.styled.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { getTypography } from "@styles/typography";
+
+export const RecommendWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
