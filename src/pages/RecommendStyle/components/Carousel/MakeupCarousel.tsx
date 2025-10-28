59309afbe820358f3dfb3abe4fd6473485bfb8ad
sinji <sinzi031012@naver.com>
2025-10-28 23:01:49 +0900

    Design: 자잘한 디자인 디테일 수정

--- src/pages/RecommendStyle/components/Carousel/MakeupCarousel.tsx
+++ src/pages/RecommendStyle/components/Carousel/MakeupCarousel.tsx
@@ -1,7 +1,6 @@
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { useLayoutEffect, useRef, useState } from "react";
 import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -22,7 +21,6 @@ interface MakeupCarouselProps {
 const MakeUpCarousel = ({ makeUps }: MakeupCarouselProps) => {
   return (
     <S.CarouselWrapper>
-      <S.PaginationStyle></S.PaginationStyle>
       <Swiper
         slidesPerView={2.2}
         slidesPerGroup={2}
@@ -33,7 +31,21 @@ const MakeUpCarousel = ({ makeUps }: MakeupCarouselProps) => {
       >
         {makeUps.map((makeUp) => (
           <SwiperSlide key={makeUp.makeUpId}>
-            <KeywordLimitedCard makeUp={makeUp} />
+            <S.Card>
+              <S.ImageBox>
+                <img src={makeUp.imageUrl} alt={makeUp.imageName} />
+              </S.ImageBox>
+
+              <S.InfoBox>
+                <S.KeywordList>
+                  {makeUp.keywords.slice(0, 2).map((keyword) => (
+                    <S.Keyword key={keyword}>{keyword}</S.Keyword>
+                  ))}
+                </S.KeywordList>
+
+                <S.Title>{makeUp.imageName || makeUp.createdAt}</S.Title>
+              </S.InfoBox>
+            </S.Card>
           </SwiperSlide>
         ))}
       </Swiper>
@@ -42,59 +54,3 @@ const MakeUpCarousel = ({ makeUps }: MakeupCarouselProps) => {
 };
 
 export default MakeUpCarousel;
-
-// 키워드가 넘치면 자동 숨김 처리 되도록 렌더링 하는 컴포넌트
-const KeywordLimitedCard = ({ makeUp }: { makeUp: MakeUp }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [visibleKeywords, setVisibleKeywords] = useState<string[]>([]);
-
-  useLayoutEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
-
-    const temp = document.createElement("div");
-    temp.style.position = "absolute";
-    temp.style.visibility = "hidden";
-    temp.style.display = "flex";
-    temp.style.gap = "0.6rem";
-    document.body.appendChild(temp);
-
-    let totalWidth = 0;
-    const allowed: string[] = [];
-
-    makeUp.keywords.forEach((k) => {
-      const span = document.createElement("span");
-      span.innerText = k;
-      span.style.padding = "0.2rem 0.8rem";
-      span.style.fontSize = "1.2rem";
-      temp.appendChild(span);
-
-      const w = span.offsetWidth + 6; // 간격 포함
-      totalWidth += w;
-
-      if (container.offsetWidth > totalWidth) {
-        allowed.push(k);
-      }
-    });
-
-    document.body.removeChild(temp);
-    setVisibleKeywords(allowed);
-  }, [makeUp.keywords]);
-
-  return (
-    <S.Card>
-      <S.ImageBox>
-        <img src={makeUp.imageUrl} alt={makeUp.imageName} />
-      </S.ImageBox>
-
-      <S.InfoBox>
-        <S.KeywordList ref={containerRef}>
-          {visibleKeywords.map((keyword) => (
-            <S.Keyword key={keyword}>{keyword}</S.Keyword>
-          ))}
-        </S.KeywordList>
-        <S.Title>{makeUp.imageName || makeUp.createdAt}</S.Title>
-      </S.InfoBox>
-    </S.Card>
-  );
-};
