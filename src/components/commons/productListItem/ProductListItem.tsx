b14c391511ba8879fe6962d20fc7906dd0bf3fb3
sinji <sinzi031012@naver.com>
2025-11-04 21:03:22 +0900

    Design: 리스트 아이템 디자인 수정

--- src/components/commons/productListItem/ProductListItem.tsx
+++ src/components/commons/productListItem/ProductListItem.tsx
@@ -3,9 +3,9 @@ import * as S from "./ProductListItem.styled";
 const ProductListItem = () => {
   return (
     <S.RecommendProductWrapper>
-      <S.ProductImage src="https://via.placeholder.com/72" alt="Product" />
+      <S.ProductImage src="https://avatars.githubusercontent.com/u/101495452?v=4" alt="Product" />
       <S.ProductInfo>
-        <S.ProductText>Sample Product Name</S.ProductText>
+        <S.ProductText className="title">Sample Product Name</S.ProductText>
         <S.ProductText>Sample Product Name</S.ProductText>
         <S.ReviewText>⭐ 3.3 (리뷰 39)</S.ReviewText>
       </S.ProductInfo>
