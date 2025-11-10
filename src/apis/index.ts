99a8259a9be3d98149caa42e0ded56c14bc68833
sinji <sinzi031012@naver.com>
2025-11-10 19:57:07 +0900

    Feat: API 기본 세팅

---
+++ src/apis/index.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+// TODO : 쿠키에서 토큰 가져올 수 있도록 수정
+const getAccessToken = (): string | null => {
+  const user = localStorage.getItem("user");
+
+  if (user) {
+    try {
+      const userObj = JSON.parse(user);
+      return userObj.accessToken || "";
+    } catch (error) {
+      console.error("Failed to parse user from localStorage", error);
+      return "";
+    }
+  }
+  return "";
+};
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+
+  headers: {
+    Authorization: `Bearer ${getAccessToken()}`,
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  const token = getAccessToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export function get<T>(...args: Parameters<typeof instance.get>) {
+  return instance.get<T>(...args);
+}
+
+export function post<T>(...args: Parameters<typeof instance.post>) {
+  return instance.post<T>(...args);
+}
+
+export function put<T>(...args: Parameters<typeof instance.put>) {
+  return instance.put<T>(...args);
+}
+
+export function patch<T>(...args: Parameters<typeof instance.patch>) {
+  return instance.patch<T>(...args);
+}
+
+export function del<T>(...args: Parameters<typeof instance.delete>) {
+  return instance.delete<T>(...args);
+}
